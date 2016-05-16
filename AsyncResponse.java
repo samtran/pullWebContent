a6fa7f8c8f28831a2b915895afb5a7d836b23e87
Samantha Tran <sammie_6495@yahoo.com>
2016-05-16 08:14:53 -0700

    Create AsyncResponse.java
    New class created to pass html data to main activity

---
+++ AsyncResponse.java
@@ -0,0 +1,10 @@
+package com.example.samanthatran.pullwebcontent;
+
+/**
+ * Created by samanthatran on 5/14/16.
+ */
+
+//Responsible for sending html code back to main activity
+public interface AsyncResponse {
+    void processFinish(String output);
+}
